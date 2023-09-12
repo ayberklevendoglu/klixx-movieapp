9b0435b46379442cd591090fe23ce13ddfc0007f
Ayberk Levendoglu <ayberklevendoglu@gmail.com>
2023-09-12 13:53:29 +0300

    added similar movies and tv shows section to detail pages

--- js/script.js
+++ js/script.js
@@ -88,61 +88,94 @@ const displayMovieDetail = async () => {
   const div = document.createElement("div");
 
   div.innerHTML = `
-  <div class="details-top">
-  <div>
-  ${
-    movie.poster_path
-      ? `<img
-    src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-    class="card-img-top"
-    alt="${movie.title}"
-  />`
-      : `<img
-  src="images/no-image.png"
-  class="card-img-top"
-  alt="${movie.title}"
-/>`
-  }
-  </div>
-  <div>
-    <h2>${movie.title}</h2>
-    <p>
-      <i class="fas fa-star text-primary"></i>
-      ${movie.vote_average.toFixed(1)}/ 10
-    </p>
-    <p class="text-muted">Release Date: ${movie.release_date}</p>
-    <p>
-      ${movie.overview}
-    </p>
-    <h5>Genres</h5>
-    <ul class="list-group">
-      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
-    </ul>
-    <a href="${
-      movie.homepage
-    }" target="_blank" class="btn">Visit Movie Homepage</a>
-  </div>
-</div>
-<div class="details-bottom">
-  <h2>Movie Info</h2>
-  <ul>
-    <li><span class="text-secondary">Budget:</span> $${Intl.NumberFormat(
-      "en-US"
-    ).format(movie.budget)}</li>
-    <li><span class="text-secondary">Revenue:</span> $${Intl.NumberFormat(
-      "en-US"
-    ).format(movie.revenue)}</li>
-    <li><span class="text-secondary">Runtime:</span> ${
-      movie.runtime
-    } minutes</li>
-    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
-  </ul>
-  <h4>Production Companies</h4>
-  <div class="list-group">${movie.production_companies.map(
-    (company) => `${company.name}`
-  )}</div>
-</div>`;
+      <div class="details-top">
+      <div>
+      ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `<img
+      src="images/no-image.png"
+      class="card-img-top"
+      alt="${movie.title}"
+      />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)}/ 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+        </ul>
+        <a href="${
+          movie.homepage
+        }" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+      </div>
+      <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${Intl.NumberFormat(
+          "en-US"
+        ).format(movie.budget)}</li>
+        <li><span class="text-secondary">Revenue:</span> $${Intl.NumberFormat(
+          "en-US"
+        ).format(movie.revenue)}</li>
+        <li><span class="text-secondary">Runtime:</span> ${
+          movie.runtime
+        } minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies.map(
+        (company) => `${company.name}`
+      )}</div>
+      </div>`;
   document.querySelector("#movie-details").appendChild(div);
+  similarMovies(movieId);
+};
+
+const similarMovies = async (movieId) => {
+  const { results } = await fetchApiData(`movie/${movieId}/similar`);
+  results.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("card");
+    movieCard.innerHTML = `
+      <a href="tv-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `<img
+      src="images/no-image.png"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+      }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Aired: ${movie.release_date}</small>
+          </p>
+      </div>
+    `;
+    document.querySelector("#similar-movies").appendChild(movieCard);
+  });
 };
 
 const displayTvDetail = async () => {
@@ -207,6 +240,39 @@ const displayTvDetail = async () => {
   )}</div>
 </div>`;
   document.querySelector("#show-details").appendChild(div);
+  similarTvShows(showId);
+};
+
+const similarTvShows = async (showId) => {
+  const { results } = await fetchApiData(`tv/${showId}/similar`);
+  results.forEach((show) => {
+    const showCard = document.createElement("div");
+    showCard.classList.add("card");
+    showCard.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+          : `<img
+      src="images/no-image.png"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+      }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Aired: ${show.first_air_date}</small>
+          </p>
+      </div>
+    `;
+    document.querySelector("#tv-similar-movies").appendChild(showCard);
+  });
 };
 
 const displayBackgroundImage = (type, backgroundPath) => {
